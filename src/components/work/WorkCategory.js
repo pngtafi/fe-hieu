42c18340193c460f235a9e57dcbb5fa295f59b23
pngtafi <phungtai99zz@gmail.com>
2025-04-17 21:36:23 +0700

    update client13 17/4

--- src/components/work/WorkCategory.js
+++ src/components/work/WorkCategory.js
@@ -2,6 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ImageManager from '../ImageManager';
 import './WorkCategory.css';
+const categoryLabelMap = {
+  'cong-nghe-va-media': 'Công nghệ – Media',
+  'spa-va-my-pham': 'Spa – Mỹ phẩm',
+  'ca-phe-va-tra': 'Cà phê – Trà',
+  'FB-va-nha-hang': 'F&B – Nhà hàng',
+  'noi-that-va-kien-truc': 'Nội thất – Kiến trúc',
+  'van-tai-va-xay-dung-va-bat-dong-san': 'Vận tải – Xây dựng – Bất động sản',
+  'du-lich-va-nghi-duong': 'Du lịch – Nghỉ dưỡng',
+  'thoi-trang-va-phu-kien': 'Thời trang – Phụ kiện',
+  'y-te-va-giao-duc': 'Y tế – Giáo dục',
+  'bao-bi': 'Bao Bì',
+  'du-an-khac': 'Dự án Khác'
+};
 
 const WorkCategory = ({ user }) => {
   const [images, setImages] = useState([]);
@@ -18,8 +31,10 @@ const WorkCategory = ({ user }) => {
   }, [category]);
 
   return (
-    <div>
-      <h2 style={{ marginTop: '8%' }}>{category.replace(/-/g, ' ').replace(/\b\w/g, c => c.toUpperCase())}</h2>
+    <div style={{ marginTop: '10%' }}>
+      <h2 style={{ textAlign: 'center', fontSize: '30px', fontWeight: 'bold' }}>
+        {categoryLabelMap[category]}
+      </h2>
       <div className="work-category">
         {images.map(img => (
           <div key={img.id} className="work-column">
