b4c83dd1d1e36b265dc171e9cab27d7e4c275f98
pngtafi <phungtai99zz@gmail.com>
2025-04-19 17:51:59 +0700

    update client12 19/4

--- src/components/ImageManager.js
+++ src/components/ImageManager.js
@@ -12,25 +12,41 @@ const ImageManager = ({
   className,
   onClick,
 }) => {
+
+  const stopAll = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const adminEvents = {
+    onClick: stopAll,
+    onMouseDown: stopAll,
+    onMouseUp: stopAll,
+    onKeyDown: stopAll,
+  };
+
   return (
     <>
       {image ? (
         <div>
           <img className={className} src={image.url} alt={type} style={{ width, ...style }} onClick={onClick} />
           {user && user.role === 'admin' && (
-            <div>
+            <div {...adminEvents}>
               <button
+                {...adminEvents}
                 onClick={(e) => {
                   stopAll(e);
                   handleDelete?.(image.id);
                 }}>Xóa</button>
-              <input type="file"
-                onClick={(e) => e.stopPropagation()}
+              <input
+                type="file"
+                {...adminEvents}
                 onChange={(e) => {
                   stopAll(e);
                   handleFileChange?.(e);
                 }} />
               <button
+                {...adminEvents}
                 onClick={(e) => {
                   stopAll(e);
                   handleUpdate?.(image.id, type);
